70eee28eb4a8d867d4b5e530a5007113e7bc5834
ARAB <ahmedmohamed23345@gmail.com>
2019-09-02 15:23:05 +0200

    ahmed

--- app/src/main/java/com/creativeshare/roses/tags/Tags.java
+++ app/src/main/java/com/creativeshare/roses/tags/Tags.java
@@ -2,7 +2,7 @@ package com.creativeshare.roses.tags;
 
 public class Tags {
 
-    public static final String base_url = "http://localhost/flower/public/";
+    public static final String base_url = "http://flower.agaaar.com/";
     public static final String IMAGE_URL = base_url+"upload/";
     public static final String IMAGE_URL2 = base_url+"storage/support/";
 
